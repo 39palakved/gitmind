0e45b0f43bc13c25707abc88948e1b3a6eb49f2d
39palakved <porwalpalak10@gmail.com>
2026-06-07 21:01:08 +0530

    new commit

--- calc.js
+++ calc.js
@@ -1,3 +1,3 @@
-let a = 5;
-let b = 10;
+let a = 11;
+let b = 13;
 const func = (a, b) => a + b;
---
+++ gemini.js
@@ -0,0 +1,68 @@
+require("dotenv").config();
+const readline = require("readline");
+const { GoogleGenAI } = require("@google/genai");
+const { execSync } = require("child_process");
+
+const ai = new GoogleGenAI({
+ apiKey: process.env.GEMINI_API_KEY,
+})
+
+async function test(){
+    const diff = execSync(
+  "git diff --cached",
+  { encoding: "utf8" }
+);
+const prompt = `
+You are an expert software engineer.
+
+Generate ONE conventional commit message.
+
+Rules:
+- Return ONLY the commit message
+- No explanation
+- No markdown
+- No options
+
+Git Diff:
+
+${diff}
+`;
+   const response = await ai.models.generateContent({
+   model: "gemini-2.5-flash",
+    contents: prompt,
+   })
+
+   const commitMessage = response.text.trim();
+
+console.log("\nSuggested Commit:");
+console.log(commitMessage);
+
+const answer = await askQuestion(
+  "\nAccept? (Y/N): "
+);
+if (answer.toLowerCase() === "y") {
+    execSync(`git commit -m "${commitMessage}"`, { stdio: "inherit" });
+     console.log("\nCommit created successfully!");
+}
+ else {
+
+  console.log("\nCommit cancelled.");
+
+}
+
+}
+function askQuestion(question) {
+  const rl = readline.createInterface({
+    input: process.stdin,
+    output: process.stdout,
+  });
+
+  return new Promise((resolve) => {
+    rl.question(question, (answer) => {
+      rl.close();
+      resolve(answer);
+    });
+  });
+}
+
+test();
